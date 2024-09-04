9313d0d5113af29323489e97d393c29fc15f5dbd
NicoDocomplexo <117663668+NicoDocomplexo@users.noreply.github.com>
2024-09-04 19:00:08 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,136 @@
+export const perguntas = [
+    {
+        enunciado: "1. Você se enxerga mais como um:",
+        alternativas: [
+            {
+                texto: "A) Herói",
+                afirmacao: [
+                    " O Superman. Você é alguém bondoso e que sempre valoriza a vida, escuta as pessoas, tenta ajudar as pessoas, não importa quem seja e está sempre a disposição"
+            ],
+                proxima: 1,
+            },
+            {
+                texto: "B)Anti-herói",
+                afirmacao: [
+                    "O Adão Negro. Você é uma pessoa justa , porém em certos ideais você presa por um equilibrio mental e esta sempre lutando por desejos e empoderando o seu valor ante a sociedade."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Para você a vingança é :",
+        alternativas: [
+            {
+                texto: "Um prato que se come frio",
+                afirmacao: [
+                    "O Adão Negro. Você é uma pessoa justa , porém em certos ideais você presa por um equilibrio mental e esta sempre lutando por desejos e empoderando o seu valor ante a sociedade.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Nunca é plena, mata a alma e envenena",
+                afirmacao: [
+                    "O Superman. Você é alguém bondoso e que sempre valoriza a vida, escuta as pessoas, tenta ajudar as pessoas, não importa quem seja e está sempre a disposição."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Em seu seu senso de justiça, não pode faltar :",
+        alternativas: [
+            {
+                texto: "Honra",
+                afirmacao: [
+                    "O Supergirl. Você é uma pessoa de bom coração, que sempre busca fazer o bem, quando posta em perigo pode ser muito violenta, mas sempre luta com muita determinação e honra."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Lealdade",
+                afirmacao: [
+                    "O Adão Negro. Você é uma pessoa justa , porém em certos ideais você presa por um equilibrio mental e esta sempre lutando por desejos e empoderando o seu valor ante a sociedade."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual poder você dominaria?",
+        alternativas: [
+            {
+                texto: "Voar",
+                afirmacao: [
+                    " O Superman. Você é alguém bondoso e que sempre valoriza a vida, escuta as pessoas, tenta ajudar as pessoas, não importa quem seja e está sempre a disposição."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Super velocidade",
+                afirmacao: [
+                    " O Flash. Você é uma pessoa descontraida e espirituosa, sendo uma pessoa boa, generosa e gentil, uma pessoa que tem uma grande responsabilidade e um otimo lider."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: " qual seria um possivel defeito seu?",
+        alternativas: [
+            {
+                texto: "Teimoso",
+                afirmacao: [
+                    " O Flash. Você é uma pessoa descontraida e espirituosa, sendo uma pessoa boa, generosa e gentil, uma pessoa que tem uma grande responsabilidade e um otimo lider."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Temperamental",
+                afirmacao: [
+                    "O Adão Negro. Você é uma pessoa justa , porém em certos ideais você presa por um equilibrio mental e esta sempre lutando por desejos e empoderando o seu valor ante a sociedade."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Você prefere um personqagem:",
+        alternativas: [
+            {
+                texto: " Sombrio e sério",
+                afirmacao: [
+                    "O Adão Negro. Você é uma pessoa justa , porém em certos ideais você presa por um equilibrio mental e esta sempre lutando por desejos e empoderando o seu valor ante a sociedade."
+                ],
+            },
+            {
+                texto: "Engraçado e descontraídos",
+                afirmacao: [
+                    " O Flash. Você é uma pessoa descontraida e espirituosa, sendo uma pessoa boa, generosa e gentil, uma pessoa que tem uma grande responsabilidade e um otimo lider."
+                    
+                ],
+                proxima: 9,
+            },
+        
+        ]
+    },
+    {
+        enunciado: "escolha um vilão:",
+        alternativas: [
+            {
+                texto: "Mr. Mxyzptlk",
+                afirmacao: [
+                    "O Supergirl. Você é uma pessoa de bom coração, que sempre busca fazer o bem, quando posta em perigo pode ser muito violenta, mas sempre luta com muita determinação e honra."
+                ],
+            },
+            {
+                texto: "Savitar",
+                afirmacao: [
+                    " O Flash. Você é uma pessoa descontraida e espirituosa, sendo uma pessoa boa, generosa e gentil, uma pessoa que tem uma grande responsabilidade e um otimo lider."
+                ],
+                proxima: 10,
+            },
+        ]
+    }
+]
